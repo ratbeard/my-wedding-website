ca3576cae7cb19171afc6b571be46fe3e410720e
Mike Frawley <frawl021@gmail.com>
2011-05-23 22:31:15 -0500

    people and images

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,10 +1,29 @@
 $(function () {
 
-  $('.people .thumbnails').delegate('img', 'click', highlightPerson)
+  $('body').delegate('.links li', 'click', changeTab)
+  
 
+  function changeTab (e) {
+    e.preventDefault()
+    // var el, id, container, current, content
 
-  function highlightPerson (e) {
+    el = $(e.currentTarget)
+    id = el.find('a').attr('href')
     
+    // Update link
+    el.siblings('.active').removeClass('active').end().addClass('active')
+    
+    // Update Content
+    container = el.closest('section')
+    old = container.find('.content .active')
+    now = container.find(id)
+    
+    old.fadeOut(function () { 
+      old.removeClass('active')
+      now.fadeIn(function () { now.addClass('active') })
+    })
+    
+    console.log(ee = el, old, now)
   }
   
 })
